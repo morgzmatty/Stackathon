fbf73219c68989fb4a25db0f234ec5dbc4a67ce9
Morgan Mattone <mpmkid@gmail.com>
2019-08-08 12:52:29 -0400

    got google maps to work, now setting up firebase

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,78 +1,104 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+import MapperScreen from "../screens/MapView";
+import SettingsScreen from "../screens/SettingsScreen";
 
 const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
+  web: { headerMode: "screen" },
+  default: {}
 });
 
 const HomeStack = createStackNavigator(
   {
-    Home: HomeScreen,
+    Home: HomeScreen
   },
   config
 );
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+  tabBarLabel: "Home",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
+      name={Platform.OS === "ios" ? "ios-home" : "md-home"}
     />
-  ),
+  )
 };
 
-HomeStack.path = '';
+HomeStack.path = "";
 
 const LinksStack = createStackNavigator(
   {
-    Links: LinksScreen,
+    Links: LinksScreen
   },
   config
 );
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: "Links",
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-link" : "md-link"}
+    />
+  )
 };
 
-LinksStack.path = '';
+LinksStack.path = "";
+
+const MapperStack = createStackNavigator(
+  {
+    Settings: MapperScreen
+  },
+  config
+);
+
+MapperStack.navigationOptions = {
+  tabBarLabel: "Map",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-map" : "md-map"}
+    />
+  )
+};
+
+MapperStack.path = "";
 
 const SettingsStack = createStackNavigator(
   {
-    Settings: SettingsScreen,
+    Settings: SettingsScreen
   },
   config
 );
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+  tabBarLabel: "Settings",
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-options" : "md-options"}
+    />
+  )
 };
 
-SettingsStack.path = '';
+SettingsStack.path = "";
 
 const tabNavigator = createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
+  MapperStack
 });
 
-tabNavigator.path = '';
+tabNavigator.path = "";
 
 export default tabNavigator;
---
+++ screens/MapView.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+export default class MapperScreen extends React.Component {
+  state = {
+    markers: []
+  };
+  //press event to add marker
+  _handleLongPress = e => {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinates: {
+            longitude: e.nativeEvent.coordinate.longitude,
+            latitude: e.nativeEvent.coordinate.latitude
+          }
+        }
+      ]
+    });
+  };
+
+  render() {
+    const { region } = this.props;
+    console.log(region);
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={MapView.PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 40.75325,
+            longitude: -74.003807,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
+          //add marker when user presses and hold map
+          onLongPress={e => this._handleLongPress(e)}
+        >
+          {this.state.markers.map((mark, i) => (
+            <MapView.Marker
+              key={i}
+              ref={this.setMarkerRef}
+              draggable
+              coordinate={mark.coordinates}
+            />
+          ))}
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
